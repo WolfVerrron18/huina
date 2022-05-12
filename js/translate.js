87cb716ad0d50ac9aaf1da078b1507ed5c629ed7
Kirill <103136389+WolfVerrron18@users.noreply.github.com>
2022-05-12 22:52:18 +0300

    Add files via upload

---
+++ js/translate.js
@@ -0,0 +1,20 @@
+let langArr = {
+    "title__letter": {
+        "br": "A Call to Brothers in Faith"
+    },
+    "first__p": {
+        "br": "The history of the Order of the Knights Templar, to which we are heirs, dates back more than several centuries. Initially, the Order of the Poor Knights of the Temple of Solomon comprised of nine brave knights, who as their goal set the protection of Christian pilgrims on their way to the Holy Land. As the years go by, the Order became the first and strongest charity institution in Christendom, saving hundreds of thousands of lives."
+    },
+    "second__p": {
+        "br": "However, the knight brothers were falsely accused and extinguished by the enviers of the Order, the tyrants who ruined a grand action. Today, when the world is mired in lies, world governments and the church are trading conscience and use religion and power for personal gain, we adhere to true spiritual ideals -, i.e., justice and honor, supporting and protecting those afflicted and oppressed. Our motto is – «Non nobis, Domine, non nobis, set tuo nomini da gloriam!»"
+    },
+    "third__p": {
+        "br": "In this time of trouble, we stand for a new order grounded in the old principles of faith in one God, our Father. We are set for exiling from the temples of the greedy false priests who gain profit from human sorrows, and rejecting the anger and depravity that reigns around!"
+    },
+    "fourth__p": {
+        "br": "Remember, it is time for salvation of your souls, as well as the souls of your children and your loved ones. Let everyone make their own contribution to the revival of the Order of the Poor Knights. Together we will get stronger, we will rise in the former greatness; and peace and prosperity will reign when people find freedom and well-being. Hear us out, because the soldiers of Christ are poor in material meaning only, though they are abundant spiritually, and are ready to uphold their ideals and principles: true faith, honor and justice!"
+    },
+    "knight": {
+        "br": "Donations to charity"
+    }
+}
